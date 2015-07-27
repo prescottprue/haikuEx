83eef424b6362d064ed54be975d25115f106a6ff
Scott Prue <prescottprue@gmail.com>
2015-07-26 23:08:03 -0400

    Image resizing now working with padding.

--- app/app.js
+++ app/app.js
@@ -30,3 +30,28 @@ angular.module('haikuEx', [
    'self'
    ]);
 })
+.directive('resize', function ($window) {
+    return function (scope, element) {
+        var w = angular.element($window);
+        console.log("$window:", $window);
+        scope.getWindowDimensions = function () {
+            return { 'h': w.innerHeight, 'w': w.innerWidth };
+        };
+        scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
+            scope.windowHeight = newValue.h;
+            scope.windowWidth = newValue.w;
+
+            scope.style = function () {
+                return { 
+                    'height': (newValue.h - 100) + 'px',
+                    'width': (newValue.w - 100) + 'px' 
+                };
+            };
+
+        }, true);
+
+        w.bind('resize', function () {
+            scope.$apply();
+        });
+    }
+})
