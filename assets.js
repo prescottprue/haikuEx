097867d2e06b6916319d6ffda6108f8db1a048cb
Scott Prue <prescottprue@gmail.com>
2015-07-27 04:38:57 -0400

    Switched angular, firebase and angularfire to cdn becuase of load timeout of angularfire in staging.

--- assets.js
+++ assets.js
@@ -8,14 +8,11 @@ module.exports = {
 		'components/nav/nav.css',
 	],
 	vendor: [
-		'bower/angular/angular.js',
 		'bower/angular-animate/angular-animate.js',
 		'bower/angular-aria/angular-aria.min.js',
 		'bower/ui-router/release/angular-ui-router.min.js',
 		'bower/angular-material/angular-material.min.js',
-		'bower/underscore/underscore-min.js',
-		'bower/firebase/firebase.js',
-		'bower/angularfire/dist/angularfire.js',
+		'bower/underscore/underscore-min.js'	
 	],
 	app: [
 		'app.js',
